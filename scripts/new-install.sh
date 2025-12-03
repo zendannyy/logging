85d8c40663ff2b5948e83095a4d2ae908ae59b79
Zen <dannyzen024@gmail.com>
2025-12-02 22:40:39 -0700

    pip

--- scripts/new-install.sh
+++ scripts/new-install.sh
@@ -7,14 +7,14 @@ OS_ID=$(. /etc/os-release && echo ${ID_LIKE:-$ID})
 if [ $OS_ID = 'fedora' ]; then
     echo "$OS_ID"
     dnf update -y --skip-broken
-    dnf install -y --skip-broken git curl wget; service wazuh-agent status
+    dnf install -y --skip-broken git curl pip wget; service wazuh-agent status
 elif [ $OS_ID = 'debian' ]; then
     echo "$OS_ID"
     apt-get update -y 
-    apt-get install -y --fix-broken git curl wget; systemctl status wazuh-agent
+    apt-get install -y --fix-broken git curl pip wget; systemctl status wazuh-agent
 elif [ $OS_ID = 'alpine' ]; then
     echo "$OS_ID"
-    apk add -y git curl wget; systemctl status wazuh-agent
+    apk add -y git curl pip wget; systemctl status wazuh-agent
 elif [ "$OS_ID" = 'Darwin' ]; then
     echo $OS_ID
     brew update
